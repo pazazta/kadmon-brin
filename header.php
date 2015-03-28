467cda2ffc388298d3488ae92c131a828c4f993c
pazazta <pazaztael@gmail.com>
2015-03-28 12:21:36 +0300

    Hello World

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 		</script>	
 </head>
 	<body>
-	<!-- Hello World -->
+	<!-- Hello World Yes -->
                 <div id="header">
                     <div id="header_content">
                         <?php
